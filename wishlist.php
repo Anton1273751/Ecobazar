df169dd643a3e0077736ded669726e01955e2afb
Anton1273751 <kronoss161@gmail.com>
2024-06-18 11:52:05 +0300

    save

--- wishlist.php
+++ wishlist.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./src/style/index.css" />
-    <script defer src ="./src/js/index.js"></script>
-    <title>Ecobazar</title>
-  </head>
-  <body>
-    <!--Шапка сайта-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="stylesheet" href="./src/style/index.css" />       
+       <script defer src = "./src/js/shop.js"></script>
+       <script defer src = "./src/js/price-hidden.js"></script>
+       <script defer src ="./src/js/rating-hidden.js"></script>
+       <script defef src ="./src/js/popular-hidden.js"></script>
+
+    <title>shop</title>
+</head>
+<body>
+     <!--Шапка сайта-->
     <header class="header">
       <div class="container cap">
         <div class="cap__location location">
@@ -65,10 +69,6 @@
       </section>
       <section class="container navigation">
         <nav class="nav">
-          
-          <select name="" id="category">
-            <option value="">All Category</option>
-          </select>
 
           <select name="" id="home">
             <option value="home">Home</option>
@@ -76,12 +76,8 @@
           <select name="Shop" id="shop" onchange="window.location.href = this.value;">
             <option>Shop</option>
             <option value="http://ecobazar/shop.php">Vegetables</option>
-            <option value="http://ecobazar/wishlist.php">Wishlist</option>
             <option value="https://www.youtube.com/watch?v=T1AMMeQhLEc">YouTube</option>
             <option value="http://ecobazar/">Home</option>
-
-
-            
           </select>
           <select name="" id="pages">
             <option value="pages">Pages</option>
@@ -97,159 +93,34 @@
           <p class="phone__num">(219) 555-0114</p>
         </div>
       </section>
-      <section class="container slider">
-        <div class="slider__left">
-          <img src="./src/images/Group.png" alt="#" />
-        </div>
-        <div class="slider__right"></div>
 
-        <img class="slider__img" src="./src/images/fructs.webp" alt="#" />
-        <div class="slider__discount discount">
-          <p class="discount__percent">70%</p>
-          <p class="discount__text">off</p>
-        </div>
-        <article class="slider__info card">
-          <p class="card__greetings">Welcome to shopery</p>
-          <h3 class="card__title">Fresh& Healthy Organic Food</h3>
-          <p class="card__deliver">
-            Free shipping on all your order. we deliver, you enjoy
-          </p>
-          <button class="card__btn">
-            Shop now
-            <img class="card__img" src="./src/images/rigth_btn.png" alt="#" />
-          </button>
-        </article>
-        <div class="slider__right">
-          <img src="./src/images/rigth.png" alt="#" />
-        </div>
-        <div class="slider__right"></div>
+      <section class= "breadcrumbs">
+        <div></div>
       </section>
-
-      <section class="container advice">
-        <div class = "advice__wrapper">
-           <?php 
-        include "./src/php/advice.php"
-        ?>
-        </div>
-   
-      </section>
-
-      <section class="product">
-        <div class ="container">
-          <h3 class = "product__title">Introducing Our Products</h3>
-        <div class ="product__link link">
-          <a  class ="link__ref" href="#">All</a>
-          <a class ="link__ref" href="#">Vegetable</a>
-          <a class ="link__ref"href="#">Fruit</a>
-          <a class ="link__ref" href="#">Meat & Fish</a>
-          <a  class ="link__ref"href="#">View All</a>
-        </div>
-        <div class ="product__wrapp">
-        <?php 
-        include "./src/php/get_products.php"
-        ?>
-       </div>
-        </div>       
       
-      </section>
-      <section class = "container performance">
-       
-          <?php 
-        include "./src/php/perfomance.php"
-        ?>
-      </section>
-      <section class = "best-deals">
-        <div class ="container deal">
-          <img class ="deal__img-product" src="./src/images/deals/vegetables.webp" alt="#">
-          <article class = "deal__card">
-            <h3 class ="deal__title" >Best Deals</h3>
-            <p class ="deal__text">Our Special Products Deal of the Month</p>
-            <div class ="deal__timer timer">
-              <div class ="timer__wrap">
-                <p class = "timer__days">00</p>
-                <p class ="timer__text">Days</p>
-              </div>
-               <div class ="timer__wrap">
-                <p class = "timer__hours"></p>
-                <p class ="timer__text">Hours</p>
-              </div>
-               <div class ="timer__wrap">
-                <p class = "timer__minuts"></p>
-                <p class ="timer__text">Mins</p>
-              </div>
-               <div class ="timer__wrap">
-                <p class = "timer__second"></p>
-                <p class ="timer__text">Secs</p>
-              </div>
-            </div>
-            <button class="deal__card-btn">Shop now <img class = "deal__arrow" src="./src/images/rigth_btn.png" alt="#"></button>
-          </article>
-          <img class ="deal__img-salerman" src="./src/images/deals/Image3.webp" alt="#">
-
-          
-        </div>
-
-      </section>
-      <section class ="container future-products">
-        <h3 class = 'future-products__headline'>Featured Products</h3>
-        <div class = "future-products__card-wrapp">
-            <?php 
-        include "./src/php/future-products.php"
-        ?>
-        </div>
-      </section>
-      <section class ="clients">
-        <div class ="container">
-        <h3 class ="clients__title">What our Clients Says</h3>
-          <div class = "clients__cards ">
-             <?php 
-            include "./src/php/clients.php"
-        ?>
-           </div>
-       </div>
-      </section>
-      <section class = "container video">
-        <div class="video__wrapp">
-           <video controls  muted loop width ="1320"  class ="video__content" src="./src/video/ECOBAZAR.mp4" poster ="./src/images/video/organic.webp"></video>
-           <div class ="video__info">
-            <h3 class ="video__title">Video</h3>
-            <p class ="video__text">We’re the Best Organic Farm in the World</p>
-           </div>          
-        </div>
-      </section>
-      <section class ="container news">
-        <h3 class="news__title">Latest News</h3>
-        <div class ="news__cards">
-            <?php 
-        include "./src/php/news.php"
-        ?>
-        </div>
-      </section>
-
-      <section class ="newsletter" >
-        <div class="newsletter__line"></div>
-        <div class="container">
-          <div class="newsletter__wrapp">
-            <div class ="newsletter__logo-wrapp">
-              <img class ="newsletter__img" src="./src/images/list.svg" alt="#">
-              <h3 class="newsletter__logo">Ecobazar</h3>
-            </div>
-            <div class ="newsletter__info">
-              <h3 class ="newsletter__title">Subcribe our Newsletter</h3>
-              <p class ="newsletter__text">Pellentesque eu nibh eget mauris congue mattis matti.</p>
-            </div>
-
-           <div class="newsletter__mail">
-            <input class="newsletter__input" type="text" placeholder ="Your email address"> 
-            <button class="newsletter__btn">Subscribe</button>
-           </div>
-            
+ 
+     
+      <section class="shop-newsletter">
+        <div class ="container shop-newsletter__wrapp">
+          <div class="shop-description"> 
+            <h3 class="shop-description__title">Subcribe our Newsletter</h3>
+            <p class="shop-description__text">Pellentesque eu nibh eget mauris congue mattis mattis nec tellus. Phasellus imperdiet elit eu magna.</p>
+          </div>    
+          <div class="shop-input__wrapp">
+           <input class="shop-input__input" type="text" placeholder ="Your email address">
+           <button class="shop-input__btn">Subscribe</button>
           </div>
+          <div class="shop-social">
+            <div class="shop-social__fasebook"><img src="./src/images/shop/social/facebook.png" alt="#"></div>
+            <img class="shop-social__twitter" src="./src/images/shop/social/twitter.png" alt="#">
+            <img class="shop-social__pinterest" src="./src/images/shop/social/pinterest.png" alt="#">
+            <img class="shop-social__instagram" src="./src/images/shop/social/instagram.png" alt="#">
+          </div>
+               
         </div>
       </section>
-    
-    </main>
-    <footer class="footer">
+</main>
+<footer class="footer">
       <div class ="container">
           <div class ="footer__info">
       <article class ="footer__shopery">
@@ -307,12 +178,7 @@
          </div>
          <div class="footer__line"></div>
          <div class = "footer__bottom bottom">
-          <div class= bottom__social-network>
-            <div class = "bottom__facebook"><img class = "bottom__facebook-img" src="./src/images/footer/facebook.png" alt="#"></div>
-            <img class ="bottom__twitter" src="./src/images/footer/twitter.png" alt="#">
-            <img class ="bottom__pinterest" src="./src/images/footer/pinterest.png" alt="#">
-            <img class ="bottom__instagram" src="./src/images/footer/instagram.png" alt="#">
-          </div>
+        
           <p class="bottom__text">Shopery eCommerce © 2021. All Rights Reserved</p>
           <div class ="bottom__pay">
              <img class ="bottom__img" src="./src/images/footer/pay/Method=ApplePay.png" alt="#">
@@ -331,5 +197,6 @@
          
        
     </footer>
-  </body>
+     
+</body>
 </html>
