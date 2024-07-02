5ee8e2629a0eea88f3f88c1d41b34ab3bb96505f
Anton1273751 <kronoss161@gmail.com>
2024-07-02 11:47:36 +0300

    save

--- src/js/count_sum_total.js
+++ src/js/count_sum_total.js
@@ -1,9 +1,16 @@
 const addCountNode = document.querySelectorAll(".shopping-cart__plus");
 const countProductNode = document.querySelectorAll(".shopping-cart__count");
-// const arcticleNode = document.q
+const articleProduct = document.querySelectorAll(".shopping-cart__article");
 const closeProduct = document.querySelectorAll(".shopping-cart__close");
 console.log(closeProduct);
 
+closeProduct.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    articleProduct.forEach((elem) => {
+      elem.remove();
+    });
+  });
+});
 
 // console.log(countProductNode);
 // let totalCount = 2;
