805f11333d083baca81a63d60561964057815faf
Anton1273751 <kronoss161@gmail.com>
2024-07-10 16:07:14 +0300

    save

--- src/js/change_quantity.js
+++ src/js/change_quantity.js
@@ -1,30 +1,25 @@
 const addCountNode = document.querySelectorAll(".shopping-cart__plus");
 const decreaseCountNode = document.querySelectorAll(".shopping-cart__minus");
-const articleNode = document.querySelectorAll(".shopping-cart__article");
 
-// articleNode.forEach((elem) => {
-//   elem.addEventListener("click", (e) => {
-//     const countElement = e.target.parentElement.querySelector(
-//       ".shopping-cart__count"
-//     ).textContent;
-//     let currentCount = parseInt(countElement);
-//     currentCount++;
-//     console.log(currentCount);
-//   });
-// });
+
+
 
 decreaseCountNode.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (button) {
+      const idElemtMinus = e.target.closest(".shopping-cart__article").id;
+      console.log(idElemtMinus);
       const countNode = e.target.parentElement.querySelector(
         ".shopping-cart__count"
       ).textContent;
       let currentCount = parseInt(countNode);
-      if (currentCount <= 0) {
+      if (currentCount <= 1) {
         e.stopPropagation();
       } else {
         currentCount--;
       }
+      console.log(currentCount);
+
       console.log("-");
     } else {
       console.log("Проблема");
@@ -35,6 +30,8 @@ decreaseCountNode.forEach((button) => {
 addCountNode.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (button) {
+      const idElemtPlus = e.target.closest(".shopping-cart__article").id;
+      console.log(idElemtPlus);
       const countNode = e.target.parentElement.querySelector(
         ".shopping-cart__count"
       ).textContent;
